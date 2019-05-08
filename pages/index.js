2be8b9eb8eee96b4f4dadd2927d9c49a64045d36
Home <home@Homes-MacBook-Pro.local>
2019-05-07 21:53:43 -0700

    first

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+`;
+
+// Dress image source
+// https://pc-ap.rtrcdn.com/productimages/back/270x/51/DOB8.jpg
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
