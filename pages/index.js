3eb6991d1018ef31152386c9884ab2139f64c706
Home <home@Homes-MacBook-Pro.local>
2019-05-15 18:52:43 -0700

    data

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,51 @@ const Div = styled.div`
 // Dress image source
 // https://pc-ap.rtrcdn.com/productimages/back/270x/51/DOB8.jpg
 
+
+[{
+	designer: 'DODO BAR OR',
+	description: 'Valeria Dress',
+	price: 105,
+	retail: 615,
+  url: 'https://pc-ap.rtrcdn.com/productimages/back/270x/51/DOB8.jpg'
+ },
+ {
+	designer: 'PETERSYN',
+	description: 'White Mina Dress',
+	price: 60,
+	retail: 385,
+  url: 'https://pc-ap.rtrcdn.com/productimages/back/270x/51/DOB8.jpg'
+ },
+ {
+	designer: 'BEC & BRIDGE',
+	description: 'Black Salut Dress',
+	price: 35,
+	retail: 220,
+  url: 'https://pc-ap.rtrcdn.com/productimages/back/270x/51/DOB8.jpg'
+ },
+ {
+	designer: 'SEA NEW YORK',
+	description: 'Brandy Dress',
+	price: 65,
+	retail: 398,
+  url: 'https://pc-ap.rtrcdn.com/productimages/back/270x/51/DOB8.jpg'
+ },
+ {
+	designer: 'ALEXIS',
+	description: 'Merlot Monet Smocked Dress',
+	price: 60,
+	retail: 385,
+  url: 'https://pc-ap.rtrcdn.com/productimages/back/270x/51/DOB8.jpg'
+ },
+ {
+	designer: 'THURLEY',
+	description: 'Venitian Nights Dress',
+	price: 120,
+	retail: 795,
+  url: 'https://pc-ap.rtrcdn.com/productimages/back/270x/51/DOB8.jpg'
+ }
+]
+
 export default class Index extends React.Component {
   render() {
     return (
